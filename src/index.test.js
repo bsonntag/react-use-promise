dbd2030587f4b515e62f5edbf0080ba4a7987080
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-02-15 21:34:11 +0000

    Add inputs array

--- src/index.test.js
+++ src/index.test.js
@@ -1,58 +1,131 @@
-import { cleanup, render, wait } from 'react-testing-library';
-import React from 'react';
+import { act, cleanup, testHook } from 'react-testing-library';
 import usePromise from '.';
 
-const Test = ({ promise }) => {
-  const [result, error, state] = usePromise(promise);
+class TestPromise {
 
-  return (
-    <>
-      <div>{state}</div>
-      <div>{String(result || error)}</div>
-    </>
-  );
-};
+  constructor() {
+    this.resolvers = new Set();
+    this.rejecters = new Set();
+  }
+
+  then(onResolve, onReject) {
+    if (onResolve) {
+      this.resolvers.add(onResolve);
+    }
+    if (onReject) {
+      this.rejecters.add(onReject);
+    }
+  }
+
+  catch(onReject) {
+    if (onReject) {
+      this.rejecters.add(onReject);
+    }
+  }
+
+  resolve(result) {
+    this.resolvers.forEach(resolver => resolver(result));
+  }
+
+  reject(error) {
+    this.rejecters.forEach(rejecter => rejecter(error));
+  }
+
+}
 
 afterEach(cleanup);
 
 test('should return a `pending` state while the promise is resolving', () => {
-  const app = <Test promise={Promise.resolve('foo')} />;
-  const { container } = render(app);
+  const promise = new TestPromise();
+  let state;
 
-  expect(container).toHaveTextContent('pending');
+  testHook(() => {
+    [,, state] = usePromise(promise, []);
+  });
+
+  expect(state).toBe('pending');
 });
 
-test('should return the resolved value', async () => {
-  const app = <Test promise={Promise.resolve('foo')} />;
-  const { container, rerender } = render(app);
+test('should return the resolved value', () => {
+  const promise = new TestPromise();
+  let state;
+  let result;
 
-  rerender(app);
-
-  await wait(() => {
-    expect(container).toHaveTextContent('resolved');
-    expect(container).toHaveTextContent('foo');
+  testHook(() => {
+    [result,, state] = usePromise(promise, []);
   });
+
+  expect(state).toBe('pending');
+
+  act(() => promise.resolve('foo'));
+
+  expect(state).toBe('resolved');
+  expect(result).toBe('foo');
 });
 
-test('should return the rejected value', async () => {
-  const app = <Test promise={() => Promise.reject('foo')} />;
-  const { container, rerender } = render(app);
+test('should return the rejected value', () => {
+  const promise = new TestPromise();
+  let state;
+  let error;
 
-  rerender(app);
-
-  await wait(() => {
-    expect(container).toHaveTextContent('rejected');
-    expect(container).toHaveTextContent('foo');
+  testHook(() => {
+    [, error, state] = usePromise(promise, []);
   });
+
+  act(() => promise.reject('foo'));
+
+  expect(state).toBe('rejected');
+  expect(error).toBe('foo');
 });
 
-test('should return null if there is no promise', async () => {
-  const app = <Test />;
-  const { container, rerender } = render(app);
+test('should return a null result if there is no promise', () => {
+  let result;
 
-  rerender(app);
-
-  await wait(() => {
-    expect(container).toHaveTextContent('null');
+  testHook(() => {
+    [result] = usePromise(null, []);
   });
+
+  expect(result).toBe(null);
+});
+
+test('should return to the pending state if the inputs change', () => {
+  const promise = new TestPromise();
+  let inputs = [false];
+  let state;
+
+  const { rerender } = testHook(() => {
+    [,, state] = usePromise(promise, inputs);
+  });
+
+  act(() => promise.resolve('foo'));
+  expect(state).toBe('resolved');
+
+  inputs = [true];
+  rerender();
+
+  expect(state).toBe('pending');
+});
+
+test('should call the callback', () => {
+  const callback = jest.fn(() => new TestPromise());
+
+  testHook(() => {
+    usePromise(callback, []);
+  });
+
+  expect(callback).toBeCalledTimes(1);
+});
+
+test('should call the callback again if the inputs change', () => {
+  const callback = jest.fn(() => new TestPromise());
+  let inputs = [false];
+
+  const { rerender } = testHook(() => {
+    usePromise(callback, inputs);
+  });
+
+  inputs = [true];
+  rerender();
+
+  expect(callback).toBeCalledTimes(2);
 });
